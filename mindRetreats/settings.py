bfb190dbbc77352676226e41f872772cb2474be1
shivansh <shivanshkate@gmail.com>
2023-01-22 16:20:04 +0530

    updated require file

--- mindRetreats/settings.py
+++ mindRetreats/settings.py
@@ -26,8 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')==TRUE
+# DEBUG = os.environ.get('DEBUG')=='TRUE'
+# DEBUG='True'
 
+DEBUG = os.environ.get('DEBUG')=='TRUE'
 ALLOWED_HOSTS = ["*",]
 
 
